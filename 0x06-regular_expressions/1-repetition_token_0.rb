ac6afc0dad0f7a2def02884d382c2eef0b9dcfb7
Sir-Blow <emmanuelakej@gmail.com>
2023-05-03 11:21:45 -0700

    commit

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# 1-repetition_token_0.rb
+puts ARGV[0].scan(/hbtt{1,4}n/).join
+
---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# 6-phone_number.rb
+puts ARGV[0].scan(/^[0-9]{10}$/).join
