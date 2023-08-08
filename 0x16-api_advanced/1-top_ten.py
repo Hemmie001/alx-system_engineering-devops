222f835576183147bcf34f10f12442de22e62fbf
Sir-Blow <emmanuelakej@gmail.com>
2023-08-08 22:05:00 +0000

    Correction 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,8 +3,7 @@
 This script writes a function that queries the Reddit API and returns the
 number of subscribers (not active users, total subscribers) for a
 given subreddit.
-If an invalid subreddit is given, the function returns 0.
-"""
+If an invalid subreddit is given, the function returns 0"""
 import json
 import requests
 
