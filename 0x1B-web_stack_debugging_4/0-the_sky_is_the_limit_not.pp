20a540e5dcb4b24aebe1c4b2c6a41220da4f3211
Sir-Blow <emmanuelakej@gmail.com>
2023-09-01 13:44:54 +0000

    0 corrected

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,14 +4,10 @@
 exec { 'fix--for-nginx':
   # Modify the ULIMIT value from 15 to 4096
   command => '/bin/sed -i "s/15/2000/" /etc/default/nginx',
-  # Specify the path for the sed command
-  path => '/usr/local/bin/:/bin/,
 }
 
 # Restart Nginx
 ->exec { 'nginx-restart':
   # Restart Nginx service
   command => '/etc/init.d/nginx restart',
-  # Specify the path for the init.d script
-  path => '/etc/init.d/',
 }
