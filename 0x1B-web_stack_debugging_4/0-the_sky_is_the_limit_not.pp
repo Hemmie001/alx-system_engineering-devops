4066cc282416b2414fbfc4469b468aaa6e266c04
Hemmie001 <emmanuelakej@gmail.com>
2023-11-16 14:43:16 +0000

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 # Increase the ULIMIT of the default Nginx file
 exec { 'fix--for-nginx':
-  # Modify the ULIMIT value from 15 to 4096
+  # Modify the ULIMIT value from 15 to 2000
   command => '/bin/sed -i "s/15/2000/" /etc/default/nginx',
 }
 
